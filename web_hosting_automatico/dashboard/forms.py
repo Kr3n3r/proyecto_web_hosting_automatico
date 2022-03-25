81e079f05c03d56dbfd983b8ac7e06a8cc75d274
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-03-25 13:40:50 +0100

    Preparado para enseñar a Mora

--- web_hosting_automatico/dashboard/forms.py
+++ web_hosting_automatico/dashboard/forms.py
@@ -12,6 +12,8 @@ for item in Servidor.SERVER_TYPES :
     item = list(item)
     server_types_choices += item
 class formulario_añadir_nuevo_servidor(forms.Form):
+    # template_name_p='../../../../web_hosting_automatico/dashboard/static/dashboard/add_new_server_as_p.html' #<- no vale
+    # template_name="dashboard/add_new_server_as_p.html" <- problema con la ruta, ya se ha copiado y adaptado la template en el index.html
     name = forms.CharField(label='Nombre del servidor:', min_length=10 ,max_length=2048, required=True, validators=[MaxLengthValidator(2048), MinLengthValidator(10)])
     cms_type = forms.ChoiceField(
         label='Tipo de CMS', 
