0c1bf49e3edc56825a241c01d6e0cee18461c5e7
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-03-28 06:49:17 +0200

    Merge branch 'main' of https://github.com/Kr3n3r/proyecto_web_hosting_automatico

--- web_hosting_automatico/dashboard/forms.py
+++ web_hosting_automatico/dashboard/forms.py
@@ -1,27 +1,30 @@
+from this import d
+from threading import local
 from django import forms
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 from .models import Servidor
 
-cms_type_choices = []
-for item in Servidor.CMS:
-    item = list(item)
-    cms_type_choices += item
-
-server_types_choices = []
-for item in Servidor.SERVER_TYPES : 
-    item = list(item)
-    server_types_choices += item
 class formulario_añadir_nuevo_servidor(forms.Form):
     # template_name_p='../../../../web_hosting_automatico/dashboard/static/dashboard/add_new_server_as_p.html' #<- no vale
     # template_name="dashboard/add_new_server_as_p.html" <- problema con la ruta, ya se ha copiado y adaptado la template en el index.html
-    name = forms.CharField(label='Nombre del servidor:', min_length=10 ,max_length=2048, required=True, validators=[MaxLengthValidator(2048), MinLengthValidator(10)])
+    name = forms.CharField(
+        label='Nombre del servidor:',
+        initial='www.webejemplo.es',
+        min_length=10,
+        max_length=2048, 
+        required=True, 
+        validators=[
+            MaxLengthValidator(2048), MinLengthValidator(10)
+            ])
     cms_type = forms.ChoiceField(
-        label='Tipo de CMS', 
+        label='Tipo de CMS',
+        # initial=0, # se hace con la intención de añadir una opción por defecto que no sea seleccionable 
+        # show_hidden_initial=True,
         choices=Servidor.CMS, 
         required=True, 
         label_suffix='', 
         error_messages={
-            'required' : 'Este campo es requerido',
+            'required' : 'Jaja, creías que me ibas a joder la query por no introducir estos datos no pedazo de puto?',
             'invalid_choice' : 'Jaja pillín, creías que podrías hackear este formulario'
             }, 
         validators=[]
@@ -30,7 +33,7 @@ class formulario_añadir_nuevo_servidor(forms.Form):
         choices=Servidor.SERVER_TYPES, 
         required=True,
         error_messages={
-            'required' : 'Este campo es requerido',
+            'required' : 'Jaja, creías que me ibas a joder la query por no introducir estos datos no pedazo de puto?',
             'invalid_choice' : 'Jaja pillín, creías que podrías hackear este formulario'
         },
         validators=[])
@@ -44,3 +47,11 @@ class formulario_añadir_nuevo_servidor(forms.Form):
     widgets = {
         'cpanel_password' : forms.PasswordInput(),
     }
+    
+    def name_is_valid(self): # thats not the way. Habría que encontrar una forma de hacer más genérico esto
+        name = self.data['name']
+        results = Servidor.objects.filter(name=name)
+        if results.count() != 0 :
+            self.errors['name'] = ['The name is not UNIQUE']
+        return self
+        
--- web_hosting_automatico/dashboard/views.py
+++ web_hosting_automatico/dashboard/views.py
@@ -77,6 +77,7 @@ class div_invalidfeedback(ErrorList):
 def añadir_nuevo_servidor(request):
     if request.method == 'POST' :
         form = formulario_añadir_nuevo_servidor(data=request.POST, error_class=div_invalidfeedback)
+        form = form.name_is_valid()
         if form.is_valid():
             return render(request, 'dashboard/index.html', {})
         else:
