e967376710d423a3076ef01d0deb9c4c961404cb
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-03-28 10:34:41 +0200

    Formateando el formulario de añadir nuevo servidor(no incluye cambio de nombres)

--- web_hosting_automatico/dashboard/forms.py
+++ web_hosting_automatico/dashboard/forms.py
@@ -2,44 +2,84 @@ from django import forms
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 from .models import Servidor
 
-cms_type_choices = []
-for item in Servidor.CMS:
-    item = list(item)
-    cms_type_choices += item
-
-server_types_choices = []
-for item in Servidor.SERVER_TYPES : 
-    item = list(item)
-    server_types_choices += item
 class formulario_añadir_nuevo_servidor(forms.Form):
     # template_name_p='../../../../web_hosting_automatico/dashboard/static/dashboard/add_new_server_as_p.html' #<- no vale
     # template_name="dashboard/add_new_server_as_p.html" <- problema con la ruta, ya se ha copiado y adaptado la template en el index.html
-    name = forms.CharField(label='Nombre del servidor:', min_length=10 ,max_length=2048, required=True, validators=[MaxLengthValidator(2048), MinLengthValidator(10)])
+    name = forms.CharField(
+        max_length=2048,
+        min_length=10,
+        strip=False,
+        empty_value='',
+        required=True, 
+        widget=None,
+        label='Nombre del servidor', 
+        initial='www.ejemplo.es',
+        # help_text='Aquí debes introducir el nombre de la URL que quieres que tu sitio adopte', se puede introducir pero se debe adaptar el html
+        error_messages={
+            'required' : 'Este campo es requerido',
+            'max_length' : 'WOW, donde vas? la longitud máxima es de 2048 caracteres, no lo digo yo, lo dicen los programadores de los buscadores',
+            'min_length' : 'WOW, un poco corta tu tula no bro? al menos 3 cm',
+            },
+        show_hidden_initial=None,
+        validators=[MaxLengthValidator(2048), MinLengthValidator(10)],
+        localize=True,
+        disabled=False,
+        label_suffix='', # se aplica a vacío para que no muestre el ':' como sufijo del label
+    )
     cms_type = forms.ChoiceField(
-        label='Tipo de CMS', 
         choices=Servidor.CMS, 
         required=True, 
-        label_suffix='', 
+        widget=None,
+        label='Tipo de CMS',
+        initial=None, # por defecto coge el primero
+        help_text='',
         error_messages={
             'required' : 'Este campo es requerido',
             'invalid_choice' : 'Jaja pillín, creías que podrías hackear este formulario'
             }, 
-        validators=[]
+        show_hidden_initial=None,
+        validators=[],
+        localize=True,
+        disabled=False,
+        label_suffix='',
     )
     server_type = forms.ChoiceField(
         choices=Servidor.SERVER_TYPES, 
-        required=True,
+        required=True, 
+        widget=None,
+        label='Tipo de servidor',
+        initial=None, # por defecto coge el primero
+        help_text='',
         error_messages={
             'required' : 'Este campo es requerido',
             'invalid_choice' : 'Jaja pillín, creías que podrías hackear este formulario'
-        },
-        validators=[])
+            }, 
+        show_hidden_initial=None,
+        validators=[],
+        localize=True,
+        disabled=False,
+        label_suffix='',
+    )
     cpanel_password = forms.CharField(
-        widget=forms.PasswordInput, 
-        required=True,
-        min_length=8,
         max_length=20, # mirar .models
-        validators=[MaxLengthValidator(20), MinLengthValidator(8)]
+        min_length=8,
+        strip=False,
+        empty_value='',
+        required=True,
+        widget=forms.PasswordInput, 
+        label='Contraseña del panel',
+        initial='',
+        help_text='',
+        error_messages={
+            'required' : 'Este campo es requerido',
+            'max_length' : 'WOW, donde vas? la longitud máxima es de 2048 caracteres, no lo digo yo, lo dicen los programadores de los buscadores',
+            'min_length' : 'WOW, un poco corta tu tula no bro? al menos 3 cm',
+            },
+        show_hidden_initial=None,
+        validators=[MaxLengthValidator(20), MinLengthValidator(8)],
+        localize=True,
+        disabled=False,
+        label_suffix='',
     )
     widgets = {
         'cpanel_password' : forms.PasswordInput(),
